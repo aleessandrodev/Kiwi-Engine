8ec84d95eef605ea58add8246fc928c4796a4b3c
Alessandro Bernardelli <136261703+aleessandrodev@users.noreply.github.com>
2026-04-01 21:42:08 +0200

    Delete _editor directory

--- _editor/functionalButtons.ts
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("myButton");
-button?.addEventListener("click", () => {
-  window.open("externalPage.html", "_blank", "width=800,height=600,menubar=no,toolbar=no,location=no,status=no");
-
-});
